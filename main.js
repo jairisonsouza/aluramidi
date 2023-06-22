4b600bc8ee930ad76135c2003d84f57e93aab0fa
jairisonsouza <jairo.tinformatica@gmail.com>
2023-06-22 11:00:29 -0300

    Configurando o som dos botões no Main JS

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    ListaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador ++;
+
+}
